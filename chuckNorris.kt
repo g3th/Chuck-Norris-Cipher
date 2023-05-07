877a353bc24cec208d7ccc239fbda2115574e9b5
g3th <theinkandpen@yahoo.co.uk>
2023-05-07 04:51:35 +0100

    first commit

---
+++ chuckNorris.kt
@@ -0,0 +1,55 @@
+fun ifTheIndexIsNotOutOfBounds(i: Int, str: String): Boolean{
+    return i-1 != -1 && i+1 != str.length
+}
+
+fun binaryConversionForCharCodes(): MutableList<String> {
+    val input = mutableListOf<String>()
+    println("Input string:")
+    readln().forEach{
+        if (Integer.toBinaryString(it.code).length == 6) {
+            input.add("0" + Integer.toBinaryString(it.code))
+        } else {
+            input.add(Integer.toBinaryString(it.code))
+        }
+    }
+    return input
+}
+
+fun chuckNorrisCipherConversion(str: String): String {
+    var result = ""
+    val resultList = mutableListOf<String>()
+    var cipher = ""
+    for (i in str.indices) {
+        when {
+            i == 0 -> {
+                if (str[i + 1] == str[i]) {
+                    result += str[i]
+                } else if (str[i+1] != str[i]) {
+                    result += str[i] + "_"
+                }
+            }
+            i == str.length - 1 -> result += str[i]
+            ifTheIndexIsNotOutOfBounds(i, str) && str[i + 1] != str[i] -> result += str[i] + "_"
+            ifTheIndexIsNotOutOfBounds(i, str) && str[i - 1] != str[i] -> result += str[i]
+            ifTheIndexIsNotOutOfBounds(i, str) && str[i + 1] == str[i] -> result += str[i]
+            ifTheIndexIsNotOutOfBounds(i, str) && str[i - 1] == str[i] -> result += str[i]
+        }
+    }
+    result.split("_").forEach{
+        resultList.add(it)
+    }
+    for (i in resultList.indices){
+        if (resultList[i].contains('1')) {
+            cipher += "0 " + "0".repeat(resultList[i].length) + " "
+        } else if (resultList[i].contains('0')){
+            cipher += "00 " + "0".repeat(resultList[i].length) + " "
+        }
+    }
+    return cipher
+}
+
+fun main() {
+    val str = binaryConversionForCharCodes()
+    val result = chuckNorrisCipherConversion(str.joinToString(separator = ""))
+    print("\nThe result:\n${result.trimEnd()}")
+}
